acdadaa1175b22edc4533d81908932d54d50be36
Eduardo <eduardooliveira7907@gmail.com>
2026-05-11 09:07:45 -0300

    implementando topbar e header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // StyleStore - Main JavaScript
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('StyleStore inicializada com sucesso.');
+    initHeader();
 });
+
+// Controle do Header ao rolar a página
+function initHeader() {
+    const header = document.querySelector('.main-header');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scroll-active');
+        } else {
+            header.classList.remove('scroll-active');
+        }
+    });
+}
